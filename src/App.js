b69fc609bb713dcc04c10c46a6589e2bc590aa5f
mdarif6 <md1arif015@gmail.com>
2023-01-15 23:52:17 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Header from "./components/pages/header/Header";
+
 import SignupPage from "./components/pages/loginpage/SignupPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <SignupPage />
     </div>
   );
